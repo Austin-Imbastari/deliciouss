fb8d72ac736c0890ff78a3cb55b9f221858cad79
Austin Imbastari <fvckimba@Austins-MBP.lan>
2022-04-12 11:17:10 -0700

    api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function Popular() {
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
+        const api = await axios.get(
+            `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=10`
+        );
+        console.log(api.data);
+    };
+
     return (
         <div>
             <h1>Popular</h1>
